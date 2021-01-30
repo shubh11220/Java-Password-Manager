c7c704853548bcc52e9ea848ad6d532905c86e08
Shubhankar Singh <shubhankarsingh2001@gmail.com>
2021-01-30 22:43:28 +0530

    updates

--- src/com/passwordmanager/verifyMaster.java
+++ src/com/passwordmanager/verifyMaster.java
@@ -1,34 +1,76 @@
 package com.passwordmanager;
 
-import javax.crypto.SecretKey;
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class verifyMaster {
+public class verifyMaster extends DBConnect{
     static String defaultMaster = "DEFAULT_KEY";
-    private static String masterHash = "";
     private static String master = defaultMaster;
     String keystorePass = "DEFAULT_KEY";
+    private static String masterHashDB = "";
+    static Scanner scan = new Scanner(System.in);
 
-//    public static boolean verifyMaster() {
-//        System.out.println("Input Master Password: ");
-//        var scan = new Scanner(System.in);
-//        master = scan.nextLine();
-//        System.out.println();
-//
-//
-//    }
-//
-//    public static void storeToKeyStore(SecretKey keyToSTore, String keystorePass, ) {
-//
-//    }
+    public static void  verifyMasterPassword() {
+        setMasterHashDB();
+        if (masterExists()) {
+            verify();
+        }
+        else resetMasterHash();
+    }
 
+
+    private static void verify() {
+        String masterInput = "";
+        System.out.println("Enter Master Password (default is 'DEFAULT_KEY') : ");
+        masterInput = scan.nextLine();
+        String inputHash = bytesToHex(getSHA(masterInput));
+        if (!inputHash.equals(getMasterHashDB())) {
+            System.out.println("Incorrect Master Password :( [Exiting]...");
+            dbCloseConnection();
+            System.exit(-1);
+        }
+        System.out.println("Congrats you now have access!");
+    }
+
+    private static void setMasterHashDB() {
+        String query = "select master from master";
+        try {
+            Statement update = connection.createStatement();
+            ResultSet rs = update.executeQuery(query);
+            rs.next();
+            masterHashDB = rs.getString(1); }
+        catch (Exception e) {
+            System.out.println("Error:(");
+        }
+    }
+    public static String getMasterHashDB() {
+        return masterHashDB;
+    }
+
+    public static void resetMasterHash() {
+        //if master is empty
+        System.out.println("Enter Master Password you want to set: ");
+        String inputMaster = scan.nextLine();
+        if (inputMaster.equals("")) inputMaster = defaultMaster;
+        String query = String.format("INSERT INTO MASTER VALUES ('%s');", bytesToHex(getSHA(inputMaster)));
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+        } catch (SQLException throwables) {
+
+        }
+    }
+
+    public static boolean masterExists() {
+        return !masterHashDB.equals("") || masterHashDB != null;
+    }
 
 
     public static byte[] getSHA(String input) {
